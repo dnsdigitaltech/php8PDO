bbddee3fd5547d4e6114e309ff349850f80edd22
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-13 17:18:16 -0300

    Script para Mensagens

--- form/enviar.php
+++ form/enviar.php
@@ -12,5 +12,7 @@
     $cabecalhos = "From: ".$email;
     mail($remetente, $assunto, $mensagem, $cabecalhos);
 ?>
-
+<<script>
+    alert('Enviado cm sucesso!')
+</script>
 <meta http-equiv="refresh" content="0; url=./">
