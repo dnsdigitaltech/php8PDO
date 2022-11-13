7957286a7feabcc950cfc191e33501a6851247c2
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-13 14:51:37 -0300

    Else e Else IF

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
     $nome = 'Davi Bernardo';
     $sobrenome = 'Santos';
-    $idade = 38;
-
+    $idade = 20;
     $numero = 10;
+    $mediaIdade = 30;
 
 
     echo $nome.' '.$sobrenome.'<br> tem '.$idade.' anos';
     echo '<p> Total: '.($idade + $numero);
 
     //TOMADAS DE DECISÃO IF
-    if($idade > 30){
-        echo '<p>Idade maior que 30</p>';
+    if($idade > $mediaIdade){
+        echo '<p>Idade maior que '.$mediaIdade.'</p>';
+    }elseif($idade == $mediaIdade){
+        echo '<p>Idade igual que '.$mediaIdade.'</p>';
+    }else{
+        echo '<p>Idade menor que '.$mediaIdade.'</p>';
     }
 ?>
