5ca051164e63e2e9b211888bcfdda728067b297e
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-13 17:45:11 -0300

    Conexão com o Banco de Dados

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+    //DEFINIR DATA E HORA COM BASE NO LOCAL SELECIONADO
+    data_default_timezone_set('America/Sao_Paulo');
+    $pdo = new PDO("mysql:dbname=php8;host=localhost","root","12345678")
+?>
