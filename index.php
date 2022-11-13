3d9e66cb6b31f58b532aa11c0df3ba3dd3393428
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-13 14:33:16 -0300

    Operações Matemáticas

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
     $nome = 'Davi Bernardo';
     $sobrenome = 'Santos';
     $idade = 38;
+
+    $numero = 10;
+
+
     echo $nome.' '.$sobrenome.'<br> tem '.$idade.' anos';
+    echo '<br> Total: '.$idade+$numero;
 ?>
