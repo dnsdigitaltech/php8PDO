52f9f84d585e85f6e738d2cd8390be99d2e0b6d5
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-13 16:46:01 -0300

    Recebendo Dados via POST

---
+++ form/enviar.php
@@ -0,0 +1,6 @@
+<?php
+    $nome       = $_POST['nome'];
+    $email      = $_POST['email'];
+    $mensagem   = $_POST['mensagem'];
+    var_dump($nome, $email,$mensagem);
+?>
--- form/index.php
+++ form/index.php
@@ -18,18 +18,18 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <div class="container">
-            <form method="GET" action="enviar.php">
+            <form method="POST" action="enviar.php">
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Nome</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Insira seu nome">
+                    <input name ='nome' type="text" class="form-control" id="exampleFormControlInput1" placeholder="Insira seu nome">
                 </div>
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    <input name ='email' type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
                 </div>
                 <div class="mb-3">
                     <label for="exampleFormControlTextarea1" class="form-label">Mensagem</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea name ='mensagem' class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
                 <button type="submit" class="btn btn-primary">Enviar</button>
             </form>
