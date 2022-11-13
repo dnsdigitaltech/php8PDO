bf9c85e4d63c42d73c63f64e43a3bcbb03076a19
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-13 17:01:36 -0300

    Função Mail do PHP

--- form/enviar.php
+++ form/enviar.php
@@ -1,9 +1,14 @@
 <?php
-    $nome       = $_GET['nome'];
-    $email      = $_GET['email'];
-    $mensagem   = $_GET['mensagem'];
+    $nome       = $_POST['nome'];
+    $email      = $_POST['email'];
+    $mensagem   = $_POST['mensagem'];
+    $assunto    = 'E-mail do site';
+    $remetente  = 'dnssites@gmail.com';
     
-    $conteudo = utf8_decode('Nome'. $nome."\r\n");
+    $conteudo = utf8_decode('Nome'. $nome."\r\n".
+                            'E-mail'. $email."\r\n".
+                            'Mensagem'. $mensagem."\r\n");
 
-
+    $cabecalhos = "From: ".$email;
+    mail($remetente, $assunto, $mensagem, $cabecalhos);
 ?>
