c69d9f8ff1985fad67e2f416efe7a1d90e74a7c2
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-14 10:46:04 -0300

    Segurança no Login

--- login/painel-adm/index.php
+++ login/painel-adm/index.php
@@ -1,4 +1,13 @@
-<?php @session_start();?>
+<?php 
+    @session_start();
+    //VERIFICA SE O USUÁRIO ESTÁ LOGADO E SE É DIFERENTE DE ADMINISTRADOR
+    if(@$_SESSION['nivel_usuario'] != 'Administrador'){
+        echo "<script language='javascript'> 
+                window.location='../../login'
+            </script>";
+    }
+?>
+
 Painel administrativo
 
 <p> <?php
