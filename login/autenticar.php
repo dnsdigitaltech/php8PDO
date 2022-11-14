e1f9bf01f887ded2a2911185cf3b8c2b3e160925
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-14 09:12:03 -0300

    Consulta no Banco de Dados

--- login/autenticar.php
+++ login/autenticar.php
@@ -1,9 +1,14 @@
 <?php
-    require_once("conexao.php");
-
+    require_once("../conexao.php");
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $query = $pdo->prepare('SELECT * FROM usuarios WHERE email = :email AND senha = :senha');
-
+   $query = $pdo->prepare("SELECT * FROM usuarios WHERE email = 
+            :email AND senha = :senha");
+            $query->bindValue(":email", $email);
+            $query->bindValue(":senha", $senha);
+            $query->execute();
+            $res = $query->fetchAll(PDO::FETCH_ASSOC);
+            $total_res = count($res);
+            echo $total_res;
 ?>
